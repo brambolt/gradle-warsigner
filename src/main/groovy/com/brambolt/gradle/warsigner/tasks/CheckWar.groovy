d68c702aee7dc5fb1490927061e1218c4820e1d8
Stefán Sigurðsson <stefan.sigurdsson@brambolt.com>
2020-07-01 08:52:33 +0200

    Apache 2.0 license.

--- src/main/groovy/com/brambolt/gradle/warsigner/tasks/CheckWar.groovy
+++ src/main/groovy/com/brambolt/gradle/warsigner/tasks/CheckWar.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2020 Brambolt ehf.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.brambolt.gradle.warsigner.tasks
 
 import org.gradle.api.DefaultTask
@@ -6,7 +22,6 @@ import org.gradle.api.Task
 import org.gradle.api.tasks.TaskAction
 
 import static com.brambolt.gradle.SpecObjects.asFile
-
 
 /**
  * <p>This task validates signing data and manifest attributes of application
